47017cc1a1afa66a0789cb2d759d49471b3e7eaa
Matias Insaurralde <matias@insaurral.de>
2017-01-31 21:36:39 +0200

    Add auth middleware sample.

--- mymiddleware.py
+++ mymiddleware.py
@@ -5,3 +5,12 @@ from gateway import TykGateway as tyk
 def MyPreMiddleware(request, session, spec):
     request.add_header('myheader', 'myvalue')
     return request, session
+
+@Hook
+def MyAuthMiddleware(request, session, metadata, spec):
+    auth_header = request.get_header('Authorization')
+    if auth_header == '47a0c79c427728b3df4af62b9228c8ae':
+        session.rate = 1000.0
+        session.per = 1.0
+        metadata["token"] = "47a0c79c427728b3df4af62b9228c8ae"
+    return request, session, metadata
